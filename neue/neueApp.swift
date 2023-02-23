7d0b1eb9612c875b936a0acfdbf92bd44b1b74c3
rahoofjamil <rahoof_1@live.de>
2023-02-23 14:14:48 +0100

    Initial Commit

---
+++ neue/neueApp.swift
@@ -0,0 +1,17 @@
+//
+//  neueApp.swift
+//  neue
+//
+//  Created by Rahoof Jamil on 23.02.23.
+//
+
+import SwiftUI
+
+@main
+struct neueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
